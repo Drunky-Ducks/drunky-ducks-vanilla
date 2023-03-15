02429b224e8e7024eb32f194cf7260a5f50bf3fe
Juan Felipe Ramirez <juanfel2024@gmail.com>
2023-03-15 11:59:37 +0100

    Eliminadas lineas de prueba en index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,2 @@
 import "./components/CocktailDetail.js";
 import "./components/CardMinimal.js";
-
-const cocktail = {
-  strDrink: "Mojito",
-  strAlcoholic: "No alcoholico",
-  strDrinkThumb: "https://img2.goodfon.com/wallpaper/nbig/b/a7/mojito-lime-fresh-mint-6900.jpg",
-  strIngredients: ["Ostia para Pedro"],
-  strMeasures: ["1"],
-  strInstructions: "Con la mano abierta"
-};
-
-const card = document.createElement("card-minimal");
-
-card.cocktail = cocktail;
-
-document.body.appendChild(card);
-
-document.body.addEventListener("show-details", (event) => {
-  const cocktail = event.detail;
-  document.body.innerHTML = "";
-  const container = document.createElement("cocktail-detail");
-  container.cocktail = cocktail;
-  document.body.appendChild(container);
-});
