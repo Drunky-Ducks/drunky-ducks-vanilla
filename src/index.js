694ed1bbe6c0183aed8e90ec88a2da1312c02501
Estefanía perez <list>
2023-03-15 12:49:22 +0000

    hemos instalado el swiper

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import "./components/CocktailDetail.js";
 import "./components/CardMinimal.js";
+
+ // core version + navigation, pagination modules:
+ import Swiper, { Navigation, Pagination } from 'swiper';
+ // import Swiper and modules styles
+ import 'swiper/css';
+ import 'swiper/css/navigation';
+ import 'swiper/css/pagination';
+
+ // init Swiper:
+ const swiper = new Swiper('.swiper', {
+   // configure Swiper to use modules
+   modules: [Navigation, Pagination],
+   ...
+ });
