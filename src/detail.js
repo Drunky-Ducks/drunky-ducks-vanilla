170f0b159fd87ebfe979b3ce2874335937b68a60
Juan Felipe Ramirez <juanfel2024@gmail.com>
2023-03-16 16:40:06 +0100

    Añadido evento a la felcha back.
    Se ha implementado que al darle a la flecha se haga un history
    back.

--- src/detail.js
+++ src/detail.js
@@ -6,3 +6,7 @@ const cocktail = JSON.parse(getCocktail());
 const container = document.createElement("cocktail-detail");
 container.cocktail = cocktail;
 document.querySelector(".container").appendChild(container);
+
+document.querySelector(".back-icon").addEventListener("click", () => {
+  history.back();
+});
